e50f72a8e89588e0f9abcd9892413a01446e589a
Mousumi <dutta.mousumi136@gmail.com>
2024-01-30 11:06:43 -0500

    Introduction to node

---
+++ 1-intro.js
@@ -0,0 +1,6 @@
+const amount = 9.5
+if(amount<10){
+ console.log('this is small number')
+ } else{
+  console.log("this is big number")  
+ }
---
+++ app.js
@@ -0,0 +1,16 @@
+// Globals: as this does not run on browser so no Window
+//__dirname: path to current directory
+//__filename: file name
+//require - function to use modules
+//module - info about current module(file)
+//process - info about env where the program is being executed
+
+//console.log(__dirname);
+//console.log(__filename);
+//console.log(require);
+//console.log(module);
+//console.log(process);
+
+setInterval(()=>{
+  console.log("Hello world"); //to stop printing type ctrl+c
+})
